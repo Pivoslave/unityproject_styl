e838a492e3947c384c9e5a959a478c600a324d25
Dan <den6135484@gmail.com>
2023-02-16 16:51:36 +0200

    commit #3
    added camera bobbling, adjusted movespeed for various conditions,

--- Unity Project/Assets/move.cs
+++ Unity Project/Assets/move.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class move : MonoBehaviour
@@ -7,12 +9,21 @@ public class move : MonoBehaviour
     Transform camera;
     float multiplier;
     float mouseSens;
-
     bool isCrouching;
-
+    bool isWalking;
     public float SprintTime = 5;
+    private float rotX, rotY; 
+    Vector3 relativecamdist; //camera distance relative to capsule
 
-    private float rotX, rotY;
+    int m = 1;
+
+
+    float intensity = 0.4f; // Скорость раскачки
+    float amplitude = 0.2f; // Высота раскачки
+
+    Vector3 nextSwayVector;
+    Vector3 nextSwayPosition;
+    Vector3 startLocalPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -22,26 +33,64 @@ public class move : MonoBehaviour
         mouseSens = 1000f;
 
         isCrouching = false;
+        isWalking = false;
 
         Vector3 rot = camera.transform.localEulerAngles; rotX = rot.x; rotY = rot.y;
+        relativecamdist = this.transform.parent.position - this.transform.position;
+
+
+        nextSwayVector = Vector3.up * amplitude;
+        nextSwayPosition = transform.localPosition + nextSwayVector;
+        startLocalPosition = transform.localPosition;
+        //StartCoroutine(camTilt());
     }
 
     // Update is called once per frame
     void Update()
     {
         // Running
-        if (Input.GetKeyDown(KeyCode.LeftShift)) multiplier *= 2.3f;
-        else if (Input.GetKeyUp(KeyCode.LeftShift)) multiplier = 0.007f;
+        if (Input.GetKeyDown(KeyCode.LeftShift)) { multiplier *= 2.3f; intensity += 0.1f; amplitude += 0.07f; }
+        else if (Input.GetKeyUp(KeyCode.LeftShift)) {multiplier = 0.007f; intensity -= 0.1f; amplitude -= 0.07f; }
 
         //Walking
-        if (Input.GetKey(KeyCode.W)){ this.transform.parent.Translate(Vector3.forward * multiplier); }   
+        if (Input.GetKey(KeyCode.W)){ 
+            if(!isCrouching) this.transform.parent.Translate(Vector3.forward * multiplier); 
+            else this.transform.parent.Translate(Vector3.forward * multiplier * 0.6f);
+            
+            intensity = 0.4f;
+            amplitude = 0.2f;
+            
+            isWalking = true;
+        }   
         
-        else if (Input.GetKey(KeyCode.S)){ this.transform.parent.Translate(Vector3.forward * (-multiplier)); }
+        else if (Input.GetKey(KeyCode.S)){ 
+           if(!isCrouching) this.transform.parent.Translate(Vector3.forward * (-multiplier) * 0.4f); 
+            else this.transform.parent.Translate(Vector3.forward * (-multiplier) * 0.4f * 0.6f);
 
-        if (Input.GetKey(KeyCode.D)){ this.transform.parent.Translate(Vector3.right * (multiplier)); }
+            amplitude = 0.08f;
+            intensity = 0.25f;
 
-        else if (Input.GetKey(KeyCode.A)){ this.transform.parent.Translate(Vector3.right * (-multiplier)); }
+            isWalking = true; }
 
+        if (Input.GetKey(KeyCode.D)){ 
+            if (!isCrouching) this.transform.parent.Translate(Vector3.right * (multiplier) * 0.5f);
+            else this.transform.parent.Translate(Vector3.right * (multiplier) * 0.5f * 0.6f);
+
+            amplitude = 0.12f;
+            intensity = 0.33f;
+
+            isWalking = true; }
+
+        else if (Input.GetKey(KeyCode.A)){ 
+            if(!isCrouching) this.transform.parent.Translate(Vector3.right * (-multiplier) * 0.5f);
+            else this.transform.parent.Translate(Vector3.right * (-multiplier) * 0.5f * 0.6f);
+
+            amplitude = 0.12f;
+            intensity = 0.33f;
+
+            isWalking = true; }
+
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A)) { isWalking = false; } 
 
         // Rotation
         float mouseX = Input.GetAxis("Mouse X");
@@ -80,8 +129,51 @@ public class move : MonoBehaviour
             else this.transform.parent.localScale = new Vector3(1, 0.5f, 1);
 
             isCrouching = !isCrouching;
-        } 
+        }
+
+        if (isWalking) // Если игрок движется
+        {
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, nextSwayPosition, intensity * Time.deltaTime);
+
+            if (Vector3.SqrMagnitude(transform.localPosition - nextSwayPosition) < 0.01f)
+            {
+                nextSwayVector = -nextSwayVector;
+
+                nextSwayPosition = startLocalPosition + nextSwayVector;
+            }
+        }
+        else
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, startLocalPosition, intensity * Time.deltaTime);
+    }
+
+    IEnumerator camTilt()
+    {
+        int m = 1;
+
+        
+
+        while (true)
+        {
+            for (int i = 0; i <= 40; i++)
+            {
+                this.transform.position = Vector3.Lerp(this.transform.position, this.transform.position + this.transform.right * 0.2f * m, 0.0025f * i);
+
+                yield return new WaitForSecondsRealtime(0.03f);
+            }
+
+            m *= -1;
+
+            for (int i = 0; i <= 80; i++)
+            {
+                this.transform.position = Vector3.Lerp(this.transform.position, this.transform.position + this.transform.right * 0.2f * m, 0.0025f * i);
+
+                yield return new WaitForSecondsRealtime(0.03f);
+            }
+
+            m *= -1;
+        }
     }
 
 
+  
 }
